90399e5ffd144f0b77fcaa13733ac716221e3dd6
LeighTrinty <95660798+LeighTrinity709@users.noreply.github.com>
2022-02-18 02:45:46 -0330

    Create webappstart.py

---
+++ webappstart.py
@@ -0,0 +1,455 @@
+#LeighTrinity
+#Feb 16 2020
+
+import termcolor
+import sys
+
+favicon= str(input("[___] Please enter favicon:  "))
+
+if favicon == '6399cc480d494bf1fcd7d16c42b1c11b':
+    print(termcolor.cprint('Penguin','blue'))
+    sys.exit(0)
+
+if favicon == 'f276b19aabcb4ae8cda4d22625c6735f':
+    print(termcolor.cprint('cgiirc (0.5.9)', 'blue'))
+    exit(0)
+
+if favicon == '09b565a51e14b721a323f0ba44b2982a':
+    print(termcolor.cprint('Google web server','blue'))
+    exit(0)
+
+if favicon == '506190fc55ceaa132f1bc305ed8472ca':
+    print(termcolor.cprint('SocialText','blue'))
+    exit(0)
+
+if favicon == '2cc15cfae55e2bb2d85b57e5b5bc3371':
+    print(termcolor.cprint('PHPwiki (1.3.14) / gforge (4.6.99+svn6496) - wiki', 'blue'))
+    exit(0)
+
+if favicon == '389a8816c5b87685de7d8d5fec96c85b':
+    print(termcolor.cprint('XOOPS cms','blue'))
+    exit(0)
+
+if favicon == 'f1876a80546b3986dbb79bad727b0374':
+    print(termcolor.cprint('NetScreen WebUI or 3Com Router','blue'))
+    exit(0)
+
+if favicon == '226ffc5e483b85ec261654fe255e60be':
+    print(termcolor.cprint('Netscape 4.1','blue'))
+    exit(0)
+
+if favicon == 'b25dbe60830705d98ba3aaf0568c456a':
+    print(termcolor.cprint('Netscape iPlanet 6.0','blue'))
+    exit(0)
+
+if favicon == '41e2c893098b3ed9fc14b821a2e14e73':
+    print(termcolor.cprint('Netscape 6.0 (AOL)','blue'))
+    exit(0)
+
+if favicon == 'a28ebcac852795fe30d8e99a23d377c1':
+    print(termcolor.cprint('SunOne 6.1','blue'))
+    exit(0)
+
+if favicon == '71e30c507ca3fa005e2d1322a5aa8fb2':
+    print(termcolor.cprint('Apache on Redhat','blue'))
+    exit(0)
+
+if favicon == 'd41d8cd98f00b204e9800998ecf8427e':
+    print(termcolor.cprint('Zero byte favicon','blue'))
+    exit(0)
+
+if favicon == 'dcea02a5797ce9e36f19b7590752563e':
+    print(termcolor.cprint('Parallels Plesk','red'))
+    exit(0)
+
+if favicon == '6f767458b952d4755a795af0e4e0aa17':
+    print(termcolor.cprint('Yahoo!','red'))
+    exit(0)
+
+if favicon == '5b0e3b33aa166c88cee57f83de1d4e55':
+    print(termcolor.cprint('DotNetNuke (http://www.dotnetnuke.com)','red'))
+    exit(0)
+
+if favicon == '7dbe9acc2ab6e64d59fa67637b1239df':
+    print(termcolor.cprint('Lotus-Domino','red'))
+    exit(0)
+if favicon == 'fa54dbf2f61bd2e0188e47f5f578f736':
+    print(termcolor.cprint('Wordpress','cyan'))
+    exit(0)
+
+if favicon == '6cec5a9c106d45e458fc680f70df91b0':
+    print(termcolor.cprint('Wordpress - obsolete version','red'))
+    exit(0)
+if favicon == '81ed5fa6453cf406d1d82233ba355b9a':
+    print(termcolor.cprint('E-zekiel','red'))
+    exit(0)
+if favicon == 'ecaa88f7fa0bf610a5a26cf545dcd3aa':
+    print(termcolor.cprint('3-byte invalid favicon: domain sellers','red'))
+    exit(0)
+
+if favicon == '33b04fb9f2ec918f5f14b41527e77f6d':
+    print(termcolor.cprint('znc (0.058)','red'))
+    exit(0)
+if favicon == '81feac35654318fb16d1a567b8b941e7':
+    print(termcolor.cprint('yaws (1.77)','red'))
+    exit(0)
+if favicon == 'f6d0a100b6dbeb5899f0975a1203fd85':
+    print(termcolor.cprint('witty (2.1.5)','red'))
+    exit(0)
+if favicon == 'ef5169b040925a716359d131afbea033':
+    print(termcolor.cprint('websvn (2.0)','red'))
+    exit(0)
+
+if favicon == '6434232d43f27ef5462ba5ba345e03df':
+    print(termcolor.cprint('znc (0.058, webadmin/skins/default)','magenta'))
+    exit(0)
+
+if favicon == 'e07c0775523271d629035dc8921dffc7':
+    print(termcolor.cprint('zoneminder (1.23.3)','magenta'))
+    exit(0)
+
+if favicon == '4eb846f1286ab4e7a399c851d7d84cca':
+    print(termcolor.cprint('Plone CMS (3.1.1)','magenta'))
+    exit(0)
+
+if favicon == 'e298e00b2ff6340343ddf2fc6212010b':
+    print(termcolor.cprint('Nessus 4.x Scanner Web Client','magenta'))
+    exit(0)
+if favicon == '240c36cd118aa1ff59986066f21015d4':
+    print(termcolor.cprint('LANCOM Systems','magenta'))
+    exit(0)
+
+if favicon == 'ceb25c12c147093dc93ac8b2c18bebff':
+    print(termcolor.cprint('COMpact 5020 VoIP','magenta'))
+    exit(0)
+
+if favicon == '05656826682ab3147092991ef5de9ef3':
+    print(termcolor.cprint('RapidShare','magenta'))
+    exit(0)
+
+if favicon == 'e19ffb2bc890f5bdca20f10bfddb288d':
+    print(termcolor.cprint('Rapid7 (NeXpose)','magenta'))
+    exit(0)
+
+if favicon == '1f8c0b08fb6b556a6587517a8d5f290b':
+    print(termcolor.cprint('owasp.org','magenta'))
+    exit(0)
+
+if favicon == '73778a17b0d22ffbb7d6c445a7947b92':
+    print(termcolor.cprint('Apache on Mac OS X','magenta'))
+    exit(0)
+
+if favicon == '799f70b71314a7508326d1d2f68f7519':
+    print(termcolor.cprint('JBoss Server','magenta'))
+    exit(0)
+
+if favicon == 'bd0f7466d35e8ba6cedd9c27110c5c41':
+    print(termcolor.cprint('Serena Collage (4.6, servlet/images/collage_app.ico)','magenta'))
+    exit(0)
+
+if favicon == 'dc0816f371699823e1e03e0078622d75':
+    print(termcolor.cprint('Aruba Network Devices (HTTP(S) login page)','magenta'))
+    exit(0)
+
+if favicon == '80656aabfafe0f3559f71bb0524c4bb3':
+    print(termcolor.cprint('Macromedia Breeze','magenta'))
+    exit(0)
+
+if favicon == 'f097f0adf2b9e95a972d21e5e5ab746d':
+    print(termcolor.cprint('Citrix Access Server','magenta'))
+    exit(0)
+
+if favicon == '28893699241094742c3c2d4196cd1acb':
+    print(termcolor.cprint('Xerox DocuShare','magenta'))
+    exit(0)
+
+if favicon == '48c02490ba335a159b99343b00decd87':
+    print(termcolor.cprint('Octeth Technologies oemPro (3.5.5.1)','white'))
+    exit(0)
+
+
+if favicon == '28893699241094742c3c2d4196cd1acb':
+    print(termcolor.cprint('Xerox DocuShare','magenta'))
+    exit(0)
+
+
+if favicon == '04d89d5b7a290334f5ce37c7e8b6a349':
+    print(termcolor.cprint('Atlassian Jira Bug Tracker','magenta'))
+    exit(0)
+
+if favicon == 'd80e364c0d3138c7ecd75bf9896f2cad':
+    print(termcolor.cprint('Apache Tomcat (6.0.18)','magenta'))
+    exit(0)
+
+if favicon == 'a6b55b93bc01a6df076483b69039ba9c':
+    print(termcolor.cprint('Fog Creek Fogbugz (6.1.44)','magenta'))
+    exit(0)
+
+
+if favicon == 'ee4a637a1257b2430649d6750cda6eba':
+    print(termcolor.cprint('Trimble Device Embedded Web Server','yellow'))
+    exit(0)
+
+if favicon == '9ceae7a3c88fc451d59e24d8d5f6f166':
+    print(termcolor.cprint('Plesk managed system','yellow'))
+    exit(0)
+
+if favicon == '69ae01d0c74570d4d221e6c24a06d73b':
+    print(termcolor.cprint(':Roku Soundbridge','yellow'))
+    exit(0)
+
+if favicon == '2e9545474ee33884b5fb8a9a0b8806dd':
+    print(termcolor.cprint('Ampache','yellow'))
+    exit(0)
+
+if favicon == '639b61409215d770a99667b446c80ea1':
+    print(termcolor.cprint('Lotus Domino Server','yellow'))
+    exit(0)
+
+if favicon == 'be6fb62815509bd707e69ee8dad874a1':
+    print(termcolor.cprint('i.LON server by Echelon','yellow'))
+    exit(0)
+
+if favicon == 'a46bc7fc42979e9b343335bdd86d1c3e':
+    print(termcolor.cprint('NetScout NGenius','yellow'))
+    exit(0)
+
+if favicon == '192decdad41179599a776494efc3e720':
+    print(termcolor.cprint('JBoss Installation','yellow'))
+    exit(0)
+
+if favicon == 'de2b6edbf7930f5dd0ffe0528b2bbcf4':
+    print(termcolor.cprint('Barracuda Spam/Virus firewall appliance','yellow'))
+    exit(0)
+
+if favicon == '386211e5c0b7d92efabd41390e0fc250':
+    print(termcolor.cprint('SparkWeb web-based collaboration client. http://www.igniterealtime.org/','yellow'))
+    exit(0)
+
+if favicon == 'f89abd3f358cb964d6b753a5a9da49cf':
+    print(termcolor.cprint('LimeSurvey','yellow'))
+    exit(0)
+
+if favicon == 'a7947b1675701f2247921cf4c2b99a78':
+    print(termcolor.cprint('Alexander Palmo Simple PHP Blog','yellow'))
+    exit(0)
+
+if favicon == '01febf7c2bd75cd15dae3aa093d80552':
+    print(termcolor.cprint('Atlassian Crucible or Fisheye','yellow'))
+    exit(0)
+
+if favicon == '1275afc920a53a9679d2d0e8a5c74054':
+    print(termcolor.cprint('Atlassian Crowd','yellow'))
+    exit(0)
+
+if favicon == '12888a39a499eb041ca42bf456aca285':
+    print(termcolor.cprint('Atlassian Confluence or Crowd','yellow'))
+    exit(0)
+
+if favicon == '3341c6d3c67ccdaeb7289180c741a965':
+    print(termcolor.cprint('Atlassian Confluence or Crowd','yellow'))
+    exit(0)
+
+if favicon == '6c1452e18a09070c0b3ed85ce7cb3917':
+    print(termcolor.cprint('Atlassian Jira','yellow'))
+    exit(0)
+
+if favicon == '43ba066789e749f9ef591dc086f3cd14':
+    print(termcolor.cprint('Atlassian Bamboo','yellow'))
+    exit(0)
+
+if favicon == 'a83dfece1c0e9e3469588f418e1e4942':
+    print(termcolor.cprint('Atlassian Bamboo','yellow'))
+    exit(0)
+
+if favicon == 'f0ee98b4394dfdab17c16245dd799204':
+    print(termcolor.cprint('Drupal','yellow'))
+    exit(0)
+
+if favicon == '7b0d4bc0ca1659d54469e5013a08d240':
+    print(termcolor.cprint('Netgear (Infrant) ReadyNAS NV+','yellow'))
+    exit(0)
+
+if favicon == 'cee40c0b35bded5e11545be22a40e363':
+    print(termcolor.cprint('OSSDL.de Openmailadmin','yellow'))
+    exit(0)
+
+if favicon == '4f88ba9f1298701251180e6b6467d43e':
+    print(termcolor.cprint('Xinit Systems Ltd. Openfiler','yellow'))
+    exit(0)
+
+if favicon == '4c3373870496151fd02a6f1185b0bb68':
+    print(termcolor.cprint('rPath Appliance Agent','yellow'))
+    exit(0)
+
+if favicon == 'b231ad66a2a9b0eb06f72c4c88973039':
+    print(termcolor.cprint('Wordpress','yellow'))
+    exit(0)
+
+if favicon == 'e1e8bdc3ce87340ab6ebe467519cf245':
+    print(termcolor.cprint('Wordpress','yellow'))
+    exit(0)
+
+if favicon == '95103d0eabcd541527a86f23b636e794':
+    print(termcolor.cprint('Wordpress Multi-User (MU)','yellow'))
+    exit(0)
+
+if favicon == '64ca706a50715e421b6c2fa0b32ed7ec':
+    print(termcolor.cprint('Parallels Plesk Control Panel','yellow'))
+    exit(0)
+
+if favicon == 'f425342764f8c356479d05daa7013c2f':
+    print(termcolor.cprint('vBulletin forum','yellow'))
+    exit(0)
+
+if favicon == '740af61c776a3cb98da3715bdf9d3fc1':
+    print(termcolor.cprint('vBulletin forum','yellow'))
+    exit(0)
+
+if favicon == 'd7ac014e83b5c4a2dea76c50eaeda662':
+    print(termcolor.cprint('Bulletin forum','yellow'))
+    exit(0)
+
+if favicon == 'a47951fb41640e7a2f5862c296e6f218':
+    print(termcolor.cprint('Plone CMS','yellow'))
+    exit(0)
+
+if favicon == '10bd6ad7b318df92d9e9bd03104d9b80':
+    print(termcolor.cprint('Plone CMS','yellow'))
+    exit(0)
+
+if favicon == 'e08333841cbe40d15b18f49045f26614:':
+    print(termcolor.cprint('21publish Blog','yellow'))
+    exit(0)
+
+if favicon == 'e2cac3fad9fa3388f639546f3ba09bc0':
+    print(termcolor.cprint('Invision Power Services IP.Board','yellow'))
+    exit(0)
+
+if favicon == '5ec8d0ecf7b505bb04ab3ac81535e062':
+    print(termcolor.cprint('Telligent Community Server','yellow'))
+    exit(0)
+
+if favicon == '83a1fd57a1e1684fafd6d2487290fdf5':
+    print(termcolor.cprint('Pligg','yellow'))
+    exit(0)
+
+if favicon == 'b7f98dd27febe36b7275f22ad73c5e84':
+    print(termcolor.cprint('MoinMoin','yellow'))
+    exit(0)
+
+if favicon == '63b982eddd64d44233baa25066db6bc1':
+    print(termcolor.cprint('Joomla!','yellow'))
+    exit(0)
+
+if favicon == '05bc6d56d8df6d668cf7e9e11319f4e6':
+    print(termcolor.cprint('Jive Forums','yellow'))
+    exit(0)
+
+if favicon == '63740175dae089e479a70c5e6591946c':
+    print(termcolor.cprint('The Lyceum Project','yellow'))
+    exit(0)
+
+if favicon == '4cbb2cfc30a089b29cd06179f9cc82ff':
+    print(termcolor.cprint('Dragonfly','yellow'))
+    exit(0)
+
+if favicon == '9187f6607b402df8bbc2aeb69a07bbca':
+    print(termcolor.cprint('XOOPS','yellow'))
+    exit(0)
+
+if favicon == '389a8816c5b87685de7d8d5fec96c85b':
+    print(termcolor.cprint('XOOPS','yellow'))
+    exit(0)
+
+if favicon == 'a1c686eb6e771878cf6040574a175933':
+    print(termcolor.cprint('CivicPlus','yellow'))
+    exit(0)
+
+if favicon == '4d7fe200d85000aea4d193a10e550d04':
+    print(termcolor.cprint('Intland Software codeBeamer','yellow'))
+    exit(0)
+
+if favicon == '1a9a1ec2b8817a2f951c9f1793c9bc54':
+    print(termcolor.cprint('Bitweaver','yellow'))
+    exit(0)
+
+if favicon == '1cc16c64d0e471607677b036b3f06b6e':
+    print(termcolor.cprint('Roller Weblogger Project','yellow'))
+    exit(0)
+
+if favicon == '7563f8c3ebd4fd4925f61df7d5ed8129':
+    print(termcolor.cprint('Holger Zimmerman Pi3Web HTTP Server','yellow'))
+    exit(0)
+
+if favicon == '7f0f918a78ca8d4d5ff21ea84f2bac68':
+    print(termcolor.cprint('SubText','yellow'))
+    exit(0)
+
+if favicon == '86e3bf076a018a23c12354e512af3b9c':
+    print(termcolor.cprint('Spyce','yellow'))
+    exit(0)
+
+if favicon == '9c003f40e63df95a2b844c6b61448310':
+    print(termcolor.cprint('DD-WRT Embedded Web Server','yellow'))
+    exit(0)
+
+if favicon == '9a9ee243bc8d08dac4448a5177882ea9':
+    print(termcolor.cprint('Dvbbs Forum','yellow'))
+    exit(0)
+
+if favicon == 'ee1169dee71a0a53c91f5065295004b7':
+    print(termcolor.cprint('ProjectPier','yellow'))
+    exit(0)
+
+if favicon == '7214637a176079a335d7ac529011f4e4':
+    print(termcolor.cprint('phpress','yellow'))
+    exit(0)
+
+if favicon == '1bf954ba2d568ec9771d35c94a6eb2dc':
+    print(termcolor.cprint('WoltLab Burning Board','yellow'))
+    exit(0)
+
+if favicon == 'ff3b533b061cee7cfbca693cc362c34a':
+    print(termcolor.cprint('Kayako SupportSuite','yellow'))
+    exit(0)
+
+if favicon == '428b23df874b41d904bbae29057bdba5':
+    print(termcolor.cprint('Comsenz Technology Ltd ECShop','yellow'))
+    exit(0)
+
+if favicon == '8757fcbdbd83b0808955f6735078a287':
+    print(termcolor.cprint('Comsenz Technology Ltd Discuz!','yellow'))
+    exit(0)
+
+if favicon == '9fac8b45400f794e0799d0d5458c092b':
+    print(termcolor.cprint('Comsenz Technology Ltd Discuz!','yellow'))
+    exit(0)
+
+if favicon == '4e370f295b96eef85449c357aad90328':
+    print(termcolor.cprint('Comsenz Technology Ltd SupeSite','yellow'))
+    exit(0)
+
+if favicon == '4cfbb29d0d83685ba99323bc0d4d3513':
+    print(termcolor.cprint('PHPWind Forums 7','yellow'))
+    exit(0)
+
+if favicon == '2df6edffca360b7a0fadc3bdf2191857':
+    print(termcolor.cprint('PIPS Technology ATZ Executive / Automatic Licence Plate Recognition (ALPR) System','yellow'))
+    exit(0)
+
+if favicon == '8c291e32e7c7c65124d19eb17bceca87':
+    print(termcolor.cprint('Schneider Electric Modicon 340 / BMX P34 CPU B','yellow'))
+    exit(0)
+
+if favicon == '6dcab71e60f0242907940f0fcda69ea5':
+    print(termcolor.cprint('Ubiquiti Ubiquiti M Series / AirOS','yellow'))
+    exit(0)
+
+if favicon == '09a1e50dc3369e031b97f38abddd10c8':
+    print(termcolor.cprint('Ubiquiti Ubiquiti M Series / AirOS','yellow'))
+    exit(0)
+
+if favicon == '7b345857204926b62951670cd17a08b7':
+    print(termcolor.cprint('AXESS TMC X1 or X2 Terminal','yellow'))
+    exit(0)
